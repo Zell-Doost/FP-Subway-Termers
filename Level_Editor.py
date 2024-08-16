2a33148f6946ac954e8b4efefe05371a67c753a6
KarBlam <167727708+KarBlam@users.noreply.github.com>
2024-08-16 21:18:06 +1000

    Add files via upload
    Update: when you click on a zero on the board, it turns into a 1. a "1" is supposed to represent a wall.

--- Level_Editor.py
+++ Level_Editor.py
@@ -15,26 +15,34 @@ def create_level(x_pos: int, y_pos: int) -> list[list[str]]:
 	return board
 
 
+def draw_level(level: list[list[str]], stdscr):
+	stdscr.clear()
+	for i in level:
+		for j in i:
+			stdscr.addstr(j)
+			#stdscr.addstr(" ")
+		stdscr.addstr("\n")
+
+
+	stdscr.refresh()
+
 
 
 def main(stdscr):
 	curses.mousemask(1)
 	level = create_level(20, 20)
 	level[0][10] = "J"
-	stdscr.clear()
-	for i in level:
-		for j in i:
-			stdscr.addstr(j)
-			stdscr.addstr(" ")
-		stdscr.addstr("\n")
-
-	stdscr.refresh()
-	stdscr.getch()
+	draw_level(level, stdscr)
+	
 	while True:
 		event = stdscr.getch()
 		if event == ord("q"): break
 		if event == curses.KEY_MOUSE:
 			click = curses.getmouse()
+			_, mouse_x, mouse_y, _, _ = click
+			if mouse_x < len(level[0]) and mouse_y < len(level):
+				level[mouse_y][mouse_x] = "1"
+				draw_level(level, stdscr)
 			stdscr.addstr(30, 30, str(click))
 			stdscr.refresh()
 	stdscr.refresh()
