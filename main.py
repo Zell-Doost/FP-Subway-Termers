31f46456f9574ab9dd4aab7cca037015e0bf8e8c
Zell-Doost <77568920+Zell-Doost@users.noreply.github.com>
2024-08-16 20:54:56 +1000

    Input and movement added.

--- main.py
+++ main.py
@@ -1,20 +1,55 @@
 import curses, os, time
 from pynput import keyboard
 from pynput.keyboard import Key
+from player import Player
 
+W, S, A, D, Q, E = 1, 2, 3, 4, 5, 6
 
-pressed = []
-
+pressed = {W: False, S: False, A: False, D: False, 
+           Q: False, E: False}
+#pressed = []
 
 def on_press(key):
-    if key not in pressed:
-        pressed.append(key)
+    #if key not in pressed:
+    #    pressed.append(key)
+    try:
+        if key.char == 'w':
+            pressed[W] = True
+        if key.char == 's':
+            pressed[S] = True
+        if key.char == 'a':
+            pressed[A] = True
+        if key.char == 'd':
+            pressed[D] = True
+        if key.char == 'q':
+            pressed[Q] = True
+        if key.char == 'e':
+            pressed[E] = True
+    except:
+        pass
 
 def on_release(key):
-    while key in pressed:
-        pressed.remove(key)
+    #while key in pressed:
+    #    pressed.remove(key)
     if key == Key.esc:
         return False
+    try:
+        if key.char == 'w':
+            pressed[W] = False
+        if key.char == 's':
+            pressed[S] = False
+        if key.char == 'a':
+            pressed[A] = False
+        if key.char == 'd':
+            pressed[D] = False
+        if key.char == 'q':
+            pressed[Q] = False
+        if key.char == 'e':
+            pressed[E] = False
+
+
+    except:
+        pass
 
 listener = keyboard.Listener(
         on_press = on_press, 
@@ -25,9 +60,14 @@ listener.start()
 def main(stdscr):
     
     stdscr.clear()
+    player = Player([0, 0])
     for _ in range(1000):
+        player.input_handle(pressed)
+        player.move()
         stdscr.addstr(0, 0, "                           ")
-        stdscr.addstr(0, 0, str(pressed))
+        stdscr.addstr(1, 0, "                           ")
+        stdscr.addstr(0, 0, str(player.inputs))
+        stdscr.addstr(1, 0, str(player.position))
         stdscr.refresh()
         time.sleep(1/60)
         
--- player.py
+++ player.py
@@ -1,11 +1,49 @@
+import math
+from pynput import keyboard
+from pynput.keyboard import Key
+
+W, S, A, D, Q, E = 1, 2, 3, 4, 5, 6
 
 
+def input_to_string(n):
+    return str(n.char)
 
 class Player:
     def __init__(self, position):
         self.position = position
+        self.angle = 0
         self.health = 100
         self.speed = 1
+        self.inputs = []
 
-    def take_input(self, inputs):
-        pass
+    def input_handle(self, inputs):
+        self.inputs = inputs
+
+    def move(self):
+
+        if self.inputs[W] == True:
+        #if self.inputs:
+            #return type(self.inputs[0])
+            self.position[0] += self.speed*math.cos(self.angle)
+            self.position[1] -= self.speed*math.sin(self.angle)
+
+        if self.inputs[S] == True:
+            self.position[0] -= self.speed*math.cos(self.angle)
+            self.position[1] += self.speed*math.sin(self.angle)
+
+        if self.inputs[A] == True:
+            self.position[0] -= self.speed*math.sin(self.angle)
+
+            self.position[1] -= self.speed*math.cos(self.angle)
+
+        if self.inputs[D] == True:
+            self.position[0] += self.speed*math.sin(self.angle)
+
+            self.position[1] += self.speed*math.cos(self.angle)
+        
+        if self.inputs[Q] == True:
+            self.angle += 0.1
+
+        if self.inputs[E] == True:
+            self.angle -= 0.1
+
